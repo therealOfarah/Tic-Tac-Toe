11f7f118dd8dd330ff59cbdc4808e5d2335d0cbe
therealofarah <omaarfaarah2019@gmail.com>
2022-06-02 17:48:27 -0400

    complete task 5

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 /*-------------------------------- Constants --------------------------------*/
-
+const winningCombos = [
+  [1, 1, 1, null, null, null, null, null, null],
+  [null, null, null, 1, 1, 1, null, null, null],
+  [null, null, null, null, null, null, 1, 1, 1],
+  [1, null, null, null, 1, null, null, 1, null],
+  [null, 1, null, null, 1, null, null, 1, null],
+  [null, null, 1, null, null, 1, null, null, 1],
+  [1, null, null, null, 1, null, null, null, 1,],
+  [null, null, 1, null, 1, null, 1, null, null,],
+]
 
 /*---------------------------- Variables (state) ----------------------------*/
 let board=[]
