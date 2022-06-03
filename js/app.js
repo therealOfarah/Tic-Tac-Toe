ff9d9046975e83482470cb7395d15a6a891bdce4
therealofarah <omaarfaarah2019@gmail.com>
2022-06-03 15:21:26 -0400

    complete task 7

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 /*-------------------------------- Constants --------------------------------*/
 const winningCombos = [
-  [1, 1, 1, null, null, null, null, null, null],
-  [null, null, null, 1, 1, 1, null, null, null],
-  [null, null, null, null, null, null, 1, 1, 1],
-  [1, null, null, null, 1, null, null, 1, null],
-  [null, 1, null, null, 1, null, null, 1, null],
-  [null, null, 1, null, null, 1, null, null, 1],
-  [1, null, null, null, 1, null, null, null, 1,],
-  [null, null, 1, null, 1, null, 1, null, null,],
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
 ]
 
 /*---------------------------- Variables (state) ----------------------------*/
@@ -21,16 +21,16 @@ const gameBoard = document.querySelector('.board')
 const squareEls =document.querySelectorAll('.square')
 const arrayOfSquareEls = Array.from(squareEls)
 const messageEl =document.querySelector("#message")
-
+const resetBtnEl = document.querySelector("#resetBtnEl")
 /*----------------------------- Event Listeners -----------------------------*/
 gameBoard.addEventListener('click', handleClick)
-
+resetBtnEl.addEventListener('click', init)
 
 /*-------------------------------- Functions --------------------------------*/
 function init(evt){
 board=[null,null,null,null,null,null,null,null,null]
 turn = 1
-winner = null
+winner = getWinner()
 render()
 }
 
@@ -44,18 +44,42 @@ board.forEach(function(element, index){
     }
   })
   if (winner === null){
-    messageEl.textContent = `its ${turn === 1 ? 'X' : 'O' }'s turn`
-  }else if(winner === T){
+    messageEl.textContent = `Its ${turn === 1 ? 'X' : 'O' }'s turn`
+  }else if(winner === "T"){
     messageEl.textContent= `Game is a tie`
   }else if (winner === turn){
     messageEl.textContent = `Congrats ${turn} you won`
   }
 }
+render()
 
 function handleClick(evt){
-  let sqIdx = evt.target.id.substring(2)
+  //with every click it will show in the console which box you clicked
+  let sqIdx = evt.target.id.substring(2);
+  //when spot is chosen on the board it will count as a turn
   board[sqIdx] = turn
+  //changes the value to either 1 or -1
   turn = turn * -1
+  // console.log(sqIdx)
+  winner = getWinner()
   render()
 }
 
+function getWinner() {
+  for (let i=0; i < winningCombos.length; i++) {
+      let sum = board[winningCombos[i][0]] + board[winningCombos[i][1]] + board[winningCombos[i][2]]
+      if (sum === 3) {
+          return 1
+      } else  if (sum === -3) {
+          return -1
+      } else if (board.includes(null) === false) {
+          return 'T'
+      }
+      return null
+  }
+}
+  
+
+
+
+// winningCombos.reduce()
