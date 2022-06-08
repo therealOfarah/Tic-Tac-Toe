1a32be7a7c779f5579951ea7cedb7c726e74e0b5
therealofarah <omaarfaarah2019@gmail.com>
2022-06-07 20:18:02 -0400

    debug

--- js/app.js
+++ js/app.js
@@ -20,7 +20,6 @@ let winner=''
 /*------------------------ Cached Element References ------------------------*/
 const gameBoard = document.querySelector('.board')
 const squareEls =document.querySelectorAll('.square')
-const arrayOfSquareEls = Array.from(squareEls)
 const messageEl =document.querySelector("#message")
 const resetBtnEl = document.querySelector("#resetBtnEl")
 /*----------------------------- Event Listeners -----------------------------*/
@@ -36,54 +35,48 @@ render()
 }
 
 init()
-function render(){
-board.forEach(function(element, index){
+function render() {
+  board.forEach(function (element, index) {
     if (element === 1) {
-      squareEls[index].textContent="🐱"
-    } else if (element === -1){
-      squareEls[index].textContent="🐶"
-    }else if(element === null){
-      squareEls[index].textContent=""
+      squareEls[index].textContent = "🐱"
+    } else if (element === -1) {
+      squareEls[index].textContent = "🐶"
+    } else if (element === null) {
+      squareEls[index].textContent = ""
     }
   })
-  if (winner === null){
-    messageEl.textContent = `Its ${turn === 1 ? '🐱' : '🐶' }'s turn`
-  }else if(winner === "T"){
-    messageEl.textContent= `Game is a tie`
-  }else if (winner === turn){
-    messageEl.textContent = `Congrats ${turn === 1 ? '🐱' : '🐶' } you won`
+  if (winner === null) {
+    messageEl.textContent = `Its ${turn === 1 ? '🐱' : '🐶'}'s turn`
+  } else if (winner === "T") {
+    messageEl.textContent = `Game is a tie`
+  } else {  // <== else statement works better here (if the other two conditions fail, we default to this)
+    messageEl.textContent = `Congrats ${turn === -1 ? '🐱' : '🐶'} you won`
   }
-  
 }
-render()
 
-function handleClick(evt){
-  //with every click it will show in the console which box you clicked
+// If condition checks two things before allowing a click.
+// the square must be empty
+// and the winner must be null
+function handleClick(evt) {
   let sqIdx = evt.target.id.substring(2);
-  //when spot is chosen on the board it will count as a turn
-  board[sqIdx] = turn
-  //changes the value to either 1 or -1
-  turn = turn * -1
-  // console.log(sqIdx)
-  winner = getWinner()
-  render()
+  if (!board[sqIdx] && winner === null) {
+    board[sqIdx] = turn
+    turn = turn * -1
+    winner = getWinner()
+    render()
+  }
 }
 
 function getWinner() {
   for (let i=0; i < winningCombos.length; i++) {
-      let sum = board[winningCombos[i][0]] + board[winningCombos[i][1]] + board[winningCombos[i][2]]
-      if (sum === 3) {
+      if (board[winningCombos[i][0]] + board[winningCombos[i][1]] + board[winningCombos[i][2]]===3) {
           return 1
-      } else  if (sum === -3) {
+      } else  if (board[winningCombos[i][0]] + board[winningCombos[i][1]] + board[winningCombos[i][2]] === -3) {
           return -1
-      } else if (board.includes(null) === false) {
-          return 'T'
+      } 
+    }
+    if (!board.includes(null)) { 
+      return "T"
       }
-  }
-  return null
-}
-  
-
-
-
-// winningCombos.reduce()
+      return null
+    }
